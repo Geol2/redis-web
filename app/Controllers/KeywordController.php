e8509d0d81c91470c814a658da01d5f7123446f8
Geol2 <big9401@gmail.com>
2023-07-26 14:56:38 +0900

    ADD function : KeywordController::list()

--- app/Controllers/KeywordController.php
+++ app/Controllers/KeywordController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use WalnutBread\Redis\RedisAdapter;
+use WalnutBread\Time\ExecTime;
 
 class KeywordController
 {
@@ -20,11 +21,35 @@ class KeywordController
         # data
         $data['score'] = $value;
         $data['spend_time'] = 0;
-        echo json_encode($data);
+        echo json_encode($data, JSON_PRETTY_PRINT);
         exit();
     }
 
     public static function list() {
+        $data = [];
+        $i = 1;
 
+        $exec_time = new ExecTime(['list']);
+        $exec_time->start();
+        # connection
+        RedisAdapter::setup($_ENV['REDIS_HOST'],$_ENV['REDIS_PORT'], $_ENV['REDIS_AUTH'], 0);
+
+        $redis = new RedisAdapter();
+        $arr = $redis->zRange('popular', -100, -1, true);
+        $arr = array_reverse($arr);
+        foreach($arr as $key => $value) {
+            $data['ranking'][] = [
+                "rank" => $i,
+                "keyword" => $key,
+                "score" => $value
+            ];
+            $i++;
+        }
+        $exec_time->end();
+        $data['spend_time'] = $exec_time->diff("list");
+
+        # data
+        echo json_encode($data, JSON_PRETTY_PRINT);
+        exit;
     }
 }
