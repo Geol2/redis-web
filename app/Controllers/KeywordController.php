942e179427b3ad10ce75c026f96468578da9f06c
Geol2 <big9401@gmail.com>
2023-07-26 12:13:12 +0900

    MOD function : KeywordController::search()

--- app/Controllers/KeywordController.php
+++ app/Controllers/KeywordController.php
@@ -12,14 +12,14 @@ class KeywordController
 
     public static function search() {
         $data = [];
-        var_dump($_POST);
+        # connection
         RedisAdapter::setup($_ENV['REDIS_HOST'],$_ENV['REDIS_PORT'], $_ENV['REDIS_AUTH'], 0);
 
         $redis = new RedisAdapter();
         $value = $redis->zScore('popular', 'amet');
         # data
         $data['score'] = $value;
-
+        $data['spend_time'] = 0;
         echo json_encode($data);
         exit();
     }
