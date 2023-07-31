c698454ba753bf42bba7a5d55ee9bc6233067f61
Geol2 <big9401@gmail.com>
2023-07-31 15:31:15 +0900

    MOD : root api

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,6 @@ Route::add('post', '/keyword/list', '\App\Controllers\KeywordController::list');
 Route::add('post', '/keyword/search', '\App\Controllers\KeywordController::search');
 
 Route::add('post', '/run', '\App\Controllers\RunController::run');
-Route::add('get', '/run/time', '\App\Controllers\RunController::time');
+Route::add('post', '/run/time', '\App\Controllers\RunController::time');
 
 Route::run();
